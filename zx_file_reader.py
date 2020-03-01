fe2773a3b2a9ff92c30e165a477f9f093d6a4892
Steve Jewkes <steve@mulus.net>
2020-03-01 17:07:18 +0000

    Added ability to pick out one block to process

--- zx_file_reader.py
+++ zx_file_reader.py
@@ -147,13 +147,14 @@ class TZXHandler(object):
         for i, block in enumerate(self.blocks):
             print("Block: {:4d} ({}) - {}".format(i, block.idstr, block.typedesc))
 
-    def dump(self):
+    def dump(self, blockid=None):
         """
         Output to stdout, the content of each block.
         """
         for i, block in enumerate(self.blocks):
-            print("Block: {:4d} ({})".format(i, block.idstr))
-            print(block.dump)
+            if i == blockid or not blockid:
+                print("Block: {:4d} ({})".format(i, block.idstr))
+                print(block.dump)
 
     def _process_header(self, blockid, typedesc):
         signature, end_of_text, major, minor = struct.unpack_from('7sBBB', self.data, self.pos)
@@ -183,13 +184,14 @@ def _main():
     Application entrypoint when executing the script directly.
     """
     parser = argparse.ArgumentParser(description='Utility for processing ZX Spectrum files.')
-    parser.add_argument('file', metavar='FILE', type=str, nargs=1, help='ZX Spectrum file to process (supports tzx only)')
-    parser.add_argument('--dump', action='store_true', help='Dump blocks to screen')
+    parser.add_argument('file', metavar='FILE', type=str, help='ZX Spectrum file to process (supports tzx only).')
+    parser.add_argument('--dump', action='store_true', help='Dump blocks to screen.')
     parser.add_argument('--list', action='store_true', help='Output list of blocks to screen. Any other optons are ignored if this is selected.')
+    parser.add_argument('--block', metavar='BLOCKID', type=int, help='Process a specific block ID.')
 
     args = parser.parse_args()
 
-    with open(args.file[0], "rb") as f:
+    with open(args.file, "rb") as f:
         data = f.read()
 
         if not TZXHandler.is_tzx(data):
@@ -202,7 +204,7 @@ def _main():
     if args.list:
         processor.summarize()
     elif args.dump:
-        processor.dump()
+        processor.dump(args.block)
 
 if __name__ == "__main__":
     _main()
